8a4ee495a657c916b644f11a09acc49daaa9180d
samanthasofiaparrariera-bit <samantha.sofia.parra.riera@estudiante.epsum.school>
2026-03-01 15:30:50 +0100

    Añadido modo auto

--- organizadorInterfaz.py
+++ organizadorInterfaz.py
@@ -11,6 +11,18 @@ import tkinter as tk
 from tkinter import messagebox
 
 
+# Lee el valor del checkbox.
+# True = campos en disabled
+# False = campos en normal
+def estado_campos():
+    if auto.get():
+        entry_extension.config(state="disabled")
+        entry_destino.config(state="disabled")
+    else:
+        entry_extension.config(state="normal")
+        entry_destino.config(state="normal")
+
+# En caso de que no existe un zip de respaldo, borra todo el contenido y extrae el zip
 def restaurar_respaldo():
     ruta = entry_ruta.get()
 
@@ -32,14 +44,56 @@ def restaurar_respaldo():
     except Exception as e:
         messagebox.showerror("Error", f"No se pudo restaurar: {e}")
 
-
+# Lee la ruta dle usuario y comprueba que existe.
+# Hace una copia de seguridad en un zip.
+# Modo Auto On: clasifica todos los archivos en carpetas según su extensión
+# Modo Auto Off: mueve los archivos según la extensión que escriba el usuario
 def organizar_archivos():
+    modoAuto = auto.get()
     ruta = entry_ruta.get()
     ext = entry_extension.get()
+    carpetaDestino = entry_destino.get()
+
 
     if not os.path.exists(ruta):
         messagebox.showerror("Error", "La ruta no existe")
         return
+
+    # modo auto
+    if modoAuto:
+        try:
+            shutil.make_archive("respaldo", 'zip', ruta)
+            messagebox.showinfo("Respaldo", "Copia de seguridad completada")
+        except:
+            messagebox.showerror("Error", "No se pudo crear la copia de seguridad")
+            return
+
+        movidos = 0
+
+        for nombre in os.listdir(ruta):
+            rutaArchivo = os.path.join(ruta, nombre)
+
+            if os.path.isfile(rutaArchivo):
+
+                # Aquí usamos endswith
+                if nombre.endswith(".jpg") or nombre.endswith(".png"):
+                    carpeta = "Imagenes"
+                elif nombre.endswith(".pdf") or nombre.endswith(".txt"):
+                    carpeta = "Documentos"
+                else:
+                    carpeta = "Otros"
+
+                destino = os.path.join(ruta, carpeta)
+
+                if not os.path.exists(destino):
+                    os.makedirs(destino)
+
+                shutil.move(rutaArchivo, os.path.join(destino, nombre))
+                movidos += 1
+
+        messagebox.showinfo("Completado", f"{movidos} archivos organizados automáticamente")
+        return
+
 
     archivos = []
     for f in os.listdir(ruta):
@@ -56,11 +110,29 @@ def organizar_archivos():
     except:
         messagebox.showerror("Error", "No se pudo crear la copia de seguridad")
         return
-//falta poner carpeta de destino
 
+
+
+    if carpetaDestino != "":
+        destino = os.path.join(ruta, carpetaDestino)
+
+        if not os.path.exists(destino):
+            os.makedirs(destino)
+
+        for n in archivos:
+            rutaOg = os.path.join(ruta, n)
+            rutaNueva = os.path.join(destino, n)
+            shutil.move(rutaOg, rutaNueva)
+
+        messagebox.showinfo("Completado", f"{len(archivos)} archivos movidos correctamente")
+    else:
+        messagebox.showinfo("Aviso", "No se indicó carpeta destino :(")
+
+# VENTANA, CAMPOS, BOTONES
 ventana = tk.Tk()
 ventana.title("Organizador de Archivos")
 ventana.geometry("400x250")
+auto = tk.BooleanVar(value=False)
 
 tk.Label(ventana, text="Ruta de la carpeta:").pack()
 entry_ruta = tk.Entry(ventana, width=50)
@@ -70,10 +142,19 @@ tk.Label(ventana, text="Extensión (ej: .jpg):").pack()
 entry_extension = tk.Entry(ventana, width=20)
 entry_extension.pack()
 
-//Falta boton de organizar
+# su checkbox de auto
+checkbox_auto = tk.Checkbutton(ventana, text="Auto", variable=auto, command=estado_campos)
+checkbox_auto.pack(pady=5)
+
+tk.Label(ventana, text="Carpeta destino (opcional):").pack()
+entry_destino = tk.Entry(ventana, width=30)
+entry_destino.pack()
+
+tk.Button(ventana, text="Organizar", command=organizar_archivos).pack(pady=15)
 tk.Button(ventana, text="Restaurar respaldo", command=restaurar_respaldo).pack()
 
 
 
-
+estado_campos()
 ventana.mainloop()
+# mainloop mantiene la ventana abierta
